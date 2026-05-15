dd7f17f2f2ed16f180cada24d07ad0da89f43e19
lunaarti16-svg <lunaarti16@gmail.com>
2026-05-14 20:49:54 -0500

    Update hw2.js

--- mis3371hw2/hw2.js
+++ mis3371hw2/hw2.js
@@ -5,4 +5,15 @@ Date last edited: 02/25/2026
 Version: 1
 Description: Homework 2 JS 
 */
+//dynamic date js code
+const d = new Date();
+let text = d.toLocaleDateString();
+document.getElementById("today").innerHTML = text;
+//name slider js code
+let slider = document.getElementById("range");
+let output = document.getElementById("range-slider");
+output.innerHTML = slider.value;
 
+slider.oninput = function () {
+    output.innerHTML = this.value;
+};
