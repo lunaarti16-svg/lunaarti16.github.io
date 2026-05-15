6dc2327006145edcaad6b3d18ec34c4c3608ff32
lunaarti16-svg <lunaarti16@gmail.com>
2026-05-14 23:18:52 -0500

    Update hw2.js

--- mis3371hw2/hw2.js
+++ mis3371hw2/hw2.js
@@ -6,15 +6,13 @@ Date last edited: 03/27/2026
 Version: 1.0
 Description:hw2 js
 */
-//dynamic date js code
+
 const d = new Date();
 let text = d.toLocaleDateString();
 document.getElementById("today").innerHTML = text;
 
 let slider = document.getElementById("range");
-let output = document.getElementById("range-slider");
+let output = document.getElementById("rangepain");
 output.innerHTML = slider.value;
 
-slider.oninput = function () {
-    output.innerHTML = this.value;
-};
+slider.oninput = function () {output.innerHTML = this.value;};
