f11d0659a399d338d41e90bdda07b03b77ce7ff0
lunaarti16-svg <lunaarti16@gmail.com>
2026-05-15 03:58:08 -0500

    Update hw2.js

--- mis3371hw2/hw2.js
+++ mis3371hw2/hw2.js
@@ -199,48 +199,49 @@ function rePassword() {
 
 function reviewInput() {
     var formcontent = document.getElementById("signup");
-    var formoutput = "<table class='output'><tr><th colspan='2'>Review all of your information:</th></tr>";
+
+    var formoutput =
+        "<table class='output'><tr><th colspan='2'>Review all of your information:</th></tr>";
 
     for (var i = 0; i < formcontent.elements.length; i++) {
         var el = formcontent.elements[i];
         var datatype = el.type;
+
         var label = document.querySelector(`label[for="${el.id}"]`);
         var displayName = label ? label.innerText : el.name;
         var value = el.value;
 
-        if (!name) continue;
+        if (!displayName) continue;
 
         switch (datatype) {
+
             case "checkbox":
                 if (el.checked) {
-                    formoutput += "<td align='right'>" + displayName + "</td>";
+                    formoutput += "<tr><td align='right'>" + displayName + "</td>";
                     formoutput += "<td class='outputdata'>&#x2713;</td></tr>";
                 }
                 break;
 
             case "radio":
                 if (el.checked) {
-                    formoutput += "<tr><td align='right'>" + name + "</td>";
+                    formoutput += "<tr><td align='right'>" + displayName + "</td>";
                     formoutput += "<td class='outputdata'>" + value + "</td></tr>";
                 }
                 break;
 
             case "range":
-                if (value !== "0") {
-                    formoutput += "<tr><td align='right'>" + name + "</td>";
-                    formoutput += "<td class='outputdata'>" + value + "</td></tr>";
-                }
+                formoutput += "<tr><td align='right'>" + displayName + "</td>";
+                formoutput += "<td class='outputdata'>" + value + "</td></tr>";
                 break;
 
             case "button":
             case "submit":
             case "reset":
-
                 break;
 
             default:
                 if (value !== "") {
-                    formoutput += "<tr><td align='right'>" + name + "</td>";
+                    formoutput += "<tr><td align='right'>" + displayName + "</td>";
                     formoutput += "<td class='outputdata'>" + value + "</td></tr>";
                 }
         }
@@ -248,6 +249,7 @@ function reviewInput() {
 
     formoutput += "</table>";
     document.getElementById("showInput").innerHTML = formoutput;
+}
 }
 
 function removeReview() {
